ad6ef443ae84582214aa8294315a9d27fedc79d6
Aur71 <111057774+Aur71@users.noreply.github.com>
2022-08-20 14:03:58 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const day = document.querySelectorAll(".day");
 const span = document.querySelectorAll("span");
 const li = document.querySelectorAll("li");
 const content = document.querySelectorAll(".content");
-const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const d = new Date();
 let today = weekday[d.getDay()].toLocaleLowerCase();
 
@@ -11,19 +11,19 @@ async function getData() {
     const response = await fetch(url);
     const data = await response.json();
 
-    for(let i = 0; i < data.length; i++) {
-        if(data[i].day == li[i].textContent) {
+    for (let i = 0; i < data.length; i++) {
+        if (data[i].day == li[i].textContent) {
             span[i].textContent = data[i].amount;
             day[i].style.height = `${Math.floor(data[i].amount) * 2}px`
         }
-        
-        if(today[0] + today[1] + today[2] == li[i].textContent) {
+
+        if (today[0] + today[1] + today[2] == li[i].textContent) {
             day[i].style.backgroundColor = "rgb(118, 181, 188)";
         }
 
         day[i].addEventListener("mouseenter", () => {
             content[i].style.display = "flex";
-            if(day[i].style.backgroundColor == "rgb(118, 181, 188)") {
+            if (day[i].style.backgroundColor == "rgb(118, 181, 188)") {
                 day[i].style.backgroundColor = "rgb(164, 220, 226)";
             } else {
                 day[i].style.backgroundColor = "rgb(238, 155, 138)";
@@ -32,13 +32,13 @@ async function getData() {
 
         day[i].addEventListener("mouseleave", () => {
             content[i].style.display = "none";
-            if( day[i].style.backgroundColor == "rgb(164, 220, 226)") {
+            if (day[i].style.backgroundColor == "rgb(164, 220, 226)") {
                 day[i].style.backgroundColor = "rgb(118, 181, 188)"
             } else {
                 day[i].style.backgroundColor = "rgb(236, 118, 95)";
             }
         })
-    } 
+    }
 }
 
 
